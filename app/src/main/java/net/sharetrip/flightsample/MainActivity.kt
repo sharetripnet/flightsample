873de1b37241264df1087c4279ebfe1edae6eb5e
jubayar <jubayar.ict@gmail.com>
2023-02-18 15:26:54 +0800

    Added meta data and Call Flight booking and history activity

--- app/src/main/java/net/sharetrip/flightsample/MainActivity.kt
+++ app/src/main/java/net/sharetrip/flightsample/MainActivity.kt
@@ -2,10 +2,15 @@ package net.sharetrip.flightsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.sharetrip.flight.booking.FlightBookingActivity
+import net.sharetrip.flight.history.FlightHistoryActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FlightBookingActivity.openFlightSearchService(this, "MyBl", "my_bl_user_token")
+        FlightHistoryActivity.openFlightHistoryService(this, "MyBl", "my_bl_user_token")
     }
 }
