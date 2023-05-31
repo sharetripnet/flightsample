12dbb8cdd67093e117572703f81cd17d1f5d324c
miq07 <miq0717@gmail.com>
2023-05-31 16:43:33 +0600

    0.9.98 version adopted

--- app/src/main/java/net/sharetrip/flightsample/MainActivity.kt
+++ app/src/main/java/net/sharetrip/flightsample/MainActivity.kt
@@ -2,21 +2,20 @@ package net.sharetrip.flightsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import net.sharetrip.flight.booking.FlightBookingActivity
-import net.sharetrip.flight.booking.STPartnerUserTokenValidationListener
-import net.sharetrip.flight.history.FlightHistoryActivity
+import net.sharetrip.flight.FlightHomeActivity
+import net.sharetrip.flight.STPartnerUserTokenValidationListener
 
 class MainActivity : AppCompatActivity(), STPartnerUserTokenValidationListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FlightBookingActivity.openFlightSearchService(this, "MyBl", "$2b$10$" + "G2h6ar4IHF2BdSuL6YdlR.a2RK/U4Ygcoof2oc0XZM52xf5DO387G", this)
+        FlightHomeActivity.openFlightSearchService(this, "MyBl", "$2b$10$" + "G2h6ar4IHF2BdSuL6YdlR.a2RK/U4Ygcoof2oc0XZM52xf5DO387G", this)
         //FlightHistoryActivity.openFlightHistoryService(this, "MyBl", "my_bl_user_token")
     }
 
     override fun response(isValid: Boolean, token: String) {
         if (!isValid)
-            FlightBookingActivity.updateUserToken(this, "valid_token")
+            FlightHomeActivity.updateUserToken(this, "valid_token")
     }
 }
