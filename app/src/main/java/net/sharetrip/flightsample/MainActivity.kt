100260031a9a3616c41d604dfaea37def26d611a
miq07 <miq0717@gmail.com>
2023-07-06 12:04:59 +0600

    Update to version 1.0.4

--- app/src/main/java/net/sharetrip/flightsample/MainActivity.kt
+++ app/src/main/java/net/sharetrip/flightsample/MainActivity.kt
@@ -1,21 +1,40 @@
 package net.sharetrip.flightsample
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import net.sharetrip.flight.FlightHomeActivity
 import net.sharetrip.flight.STPartnerUserTokenValidationListener
+import java.util.Locale
 
 class MainActivity : AppCompatActivity(), STPartnerUserTokenValidationListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val config = resources.configuration
+        val lang = "bn_BD" // your language code
+        val locale = Locale(lang)
+        Locale.setDefault(locale)
+        config.locale = locale
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            createConfigurationContext(config)
+        resources.updateConfiguration(config, resources.displayMetrics)
+
         setContentView(R.layout.activity_main)
 
-        FlightHomeActivity.openFlightSearchService(this, "MyBl", "$2b$10$" + "G2h6ar4IHF2BdSuL6YdlR.a2RK/U4Ygcoof2oc0XZM52xf5DO387G", this)
-        //FlightHistoryActivity.openFlightHistoryService(this, "MyBl", "my_bl_user_token")
+        findViewById<TextView>(R.id.tvOpenSDK).setOnClickListener {
+            Log.d("locale", locale.displayName)
+            FlightHomeActivity.openFlightSearchService(this, "MyBl", "$2b$10$" + "02K30P2QHS82GhyEMHemZu793XoYhTiYs/fOSx.w83.O3ZLCaoPB6", this)
+            //FlightHistoryActivity.openFlightHistoryService(this, "MyBl", "my_bl_user_token")
+        }
     }
 
     override fun response(isValid: Boolean, token: String) {
         if (!isValid)
-            FlightHomeActivity.updateUserToken(this, "valid_token")
+            FlightHomeActivity.updateUserToken(this, token)
     }
 }
